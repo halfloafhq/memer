81a2005a7eaf001227eed56f4001d65ee2fcf08a
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2024-06-10 01:42:28 +0530

    change: redirects

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,12 +7,18 @@ import { User } from "lucide-react";
 import { useClerk, useUser } from "@clerk/nextjs";
 import { useEffect, useState } from "react";
 import { useToast } from "./ui/use-toast";
+import { useRouter } from "next/navigation";
 
 export default function Navbar() {
   const { signOut } = useClerk();
   const { user } = useUser();
   const [signedIn, setSignedIn] = useState<boolean>(false);
   const { toast } = useToast();
+  const router = useRouter();
+
+  function dashboard() {
+    router.push("/dashboard")
+  }
 
   async function logout() {
     await signOut({ redirectUrl: "/" });
@@ -31,7 +37,7 @@ export default function Navbar() {
 
   return (
     <header className="flex h-16 w-full shrink-0 items-center px-4 md:px-6 bg-gray-950 text-gray-50">
-      <Link href="#" className="flex items-center gap-2" prefetch={false}>
+      <Link href="/" className="flex items-center gap-2" prefetch={false}>
         <MemerIcon className="h-6 w-6" />
         <span className="text-lg font-semibold">Memer</span>
       </Link>
@@ -45,8 +51,7 @@ export default function Navbar() {
               </Avatar>
             </DropdownMenuTrigger>
             <DropdownMenuContent align="end">
-              <DropdownMenuItem>My Account</DropdownMenuItem>
-              <DropdownMenuItem>Settings</DropdownMenuItem>
+              <DropdownMenuItem onClick={dashboard}>Dashboard</DropdownMenuItem>
               <DropdownMenuSeparator />
               <DropdownMenuItem onClick={logout}>Logout</DropdownMenuItem>
             </DropdownMenuContent>
