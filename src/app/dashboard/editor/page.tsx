d796cb88898b0d2ffe14555a23eec27e1ceb0bda
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2024-06-24 16:10:05 +0530

    add: loading state

--- src/app/dashboard/editor/page.tsx
+++ src/app/dashboard/editor/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 import dynamic from "next/dynamic";
+import { useEffect, useState } from "react";
+import Loader from "../_components/Loader";
 const Excalidraw = dynamic(
   async () => (await import("@excalidraw/excalidraw")).Excalidraw,
   {
@@ -7,5 +9,20 @@ const Excalidraw = dynamic(
   },
 );
 export default function EditorPage() {
-  return <Excalidraw />;
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  useEffect(() => {
+    setIsLoading(false);
+  }, []);
+
+  return (
+    <div className="flex flex-col h-screen">
+      {isLoading ? (
+        <div className="flex items-center justify-center h-screen">
+          <Loader />
+        </div>
+      ) : (
+        <Excalidraw />
+      )}
+    </div>
+  );
 }
