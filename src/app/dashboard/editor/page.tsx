9fe8328b857ef9b3d7274f05f77248d8f73ba390
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2024-06-24 15:58:47 +0530

    integrate: excalidraw

--- src/app/dashboard/editor/page.tsx
+++ src/app/dashboard/editor/page.tsx
@@ -1,7 +1,11 @@
+"use client";
+import dynamic from "next/dynamic";
+const Excalidraw = dynamic(
+  async () => (await import("@excalidraw/excalidraw")).Excalidraw,
+  {
+    ssr: false,
+  },
+);
 export default function EditorPage() {
-  return (
-    <div className="flex flex-col items-center justify-center">
-      <h1 className="text-3xl font-bold">We&apos;re cooking...</h1>
-    </div>
-  );
+  return <Excalidraw />;
 }
