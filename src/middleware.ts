de1d078126b88e8412fd4b1e8e82b97818f1a48b
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2024-07-04 23:50:10 +0530

    fix: public routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/", "/api/uploadthing"]);
+const isPublicRoute = createRouteMatcher(["/", "/api/uploadthing", "/sign-in(.*)", "/sign-up(.*)"]);
 
 export default clerkMiddleware((auth, req) => {
   if (!isPublicRoute(req)) {
