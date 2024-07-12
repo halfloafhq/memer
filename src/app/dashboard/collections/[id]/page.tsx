060aed7f85d92266c327238f3aeee7cdc1a03c7b
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2024-07-12 21:48:53 +0530

    change: collections id ui

--- src/app/dashboard/collections/[id]/page.tsx
+++ src/app/dashboard/collections/[id]/page.tsx
@@ -51,7 +51,8 @@ export default function CollectionPage({ params }: { params: { id: string } }) {
             className="flex items-center text-gray-600 hover:text-gray-800 transition-colors duration-200 dark:text-white"
           >
             <ChevronLeft className="w-5 h-5 mr-1" />
-            <span className="text-xl">Back to Collections</span>
+            <span className="hidden sm:block text-xl">Back to Collections</span>
+            <span className="block sm:hidden text-lg">Back</span>
           </Link>
         </div>
       ) : (
@@ -65,7 +66,8 @@ export default function CollectionPage({ params }: { params: { id: string } }) {
               className="flex items-center text-gray-600 hover:text-gray-800 transition-colors duration-200 dark:text-white"
             >
               <ChevronLeft className="w-5 h-5 mr-1" />
-              <span className="text-xl">Back to Collections</span>
+            <span className="hidden sm:block text-xl">Back to Collections</span>
+            <span className="block sm:hidden text-lg">Back</span>
             </Link>
           </div>
           <div className="mt-8">
