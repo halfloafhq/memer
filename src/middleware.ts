b5d7471ef05c63d2b45e38fe1ef06ab10011a808
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2024-07-21 14:45:25 +0530

    add: clerk webhook

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/", "/api/uploadthing", "/sign-in(.*)", "/sign-up(.*)", "/forgot-password(.*)"]);
+const isPublicRoute = createRouteMatcher(["/", "/api/uploadthing", "/sign-in(.*)", "/sign-up(.*)", "/forgot-password(.*)", "/api/webhook"]);
 
 export default clerkMiddleware((auth, req) => {
   if (!isPublicRoute(req)) {
