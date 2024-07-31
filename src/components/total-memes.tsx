ab4871d486dfe5a935cb1e68b3b448793bf7ac67
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2024-07-31 15:25:44 +0530

    add: loader

--- src/components/total-memes.tsx
+++ src/components/total-memes.tsx
@@ -3,14 +3,28 @@ import React, { useEffect, useState } from "react";
 import { Card, CardContent } from "@/components/ui/card";
 import { ImageIcon } from "lucide-react";
 import { getTotalMemes } from "@/app/_actions";
+import { Spinner } from "./spinner";
+import { useToast } from "./ui/use-toast";
 
 export function TotalMemes() {
+  const { toast } = useToast();
+  const [loading, setLoading] = useState(true);
   const [total, setTotal] = useState(0);
 
   useEffect(() => {
-    getTotalMemes().then((res) => {
-      setTotal(res);
-    });
+    try {
+      getTotalMemes().then((res) => {
+        setTotal(res);
+        setLoading(false);
+      });
+    } catch (error) {
+      setTotal(0);
+      toast({
+        title: "Error",
+        description: "Failed to fetch total memes",
+        variant: "destructive",
+      });
+    }
   }, []);
   return (
     <Card className="w-full max-w-sm mx-auto mt-8">
@@ -21,9 +35,15 @@ export function TotalMemes() {
             <p className="text-sm font-medium text-gray-500 dark:text-gray-300">
               Total Memes
             </p>
-            <p className="text-2xl font-bold text-gray-900 dark:text-white">
-              {total}
-            </p>
+            {loading ? (
+              <div className="flex items-center justify-start">
+                <Spinner />
+              </div>
+            ) : (
+              <p className="text-2xl font-bold text-gray-900 dark:text-white">
+                {total}
+              </p>
+            )}
           </div>
         </div>
       </CardContent>
