c4a3efc940f79a663f3a613409414447fb9fb611
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2024-08-01 15:22:27 +0530

    add: save editor state to localStorage

--- src/app/dashboard/editor/page.tsx
+++ src/app/dashboard/editor/page.tsx
@@ -2,17 +2,59 @@
 import dynamic from "next/dynamic";
 import { useEffect, useState } from "react";
 import Loader from "@/components/loader";
+import { AppState, BinaryFiles } from "@excalidraw/excalidraw/types/types";
+import { ExcalidrawElement } from "@excalidraw/excalidraw/types/element/types";
+
 const Excalidraw = dynamic(
   async () => (await import("@excalidraw/excalidraw")).Excalidraw,
   {
     ssr: false,
   },
 );
+
+interface EditorData {
+  elements: readonly ExcalidrawElement[];
+  appState: AppState;
+  files: BinaryFiles;
+  scrollToContent: boolean;
+}
+
 export default function EditorPage() {
   const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [initialData, setInitialData] = useState<EditorData | null>(null);
+
   useEffect(() => {
+    if (!localStorage.getItem("MEMER_EXCALIDRAW_EDITOR_DATA")) {
+      localStorage.setItem("MEMER_EXCALIDRAW_EDITOR_DATA", JSON.stringify({}));
+    }
+    const data = JSON.parse(
+      localStorage.getItem("MEMER_EXCALIDRAW_EDITOR_DATA") || "{}",
+    );
+    if (data) {
+      setInitialData({
+        ...data,
+        appState: { ...data.appState, collaborators: [] },
+        scrollToContent: true,
+      });
+    } else {
+      setInitialData(null);
+    }
     setIsLoading(false);
   }, []);
+
+  const onChange = (
+    excalidrawElements: readonly ExcalidrawElement[],
+    appState: AppState,
+    files: BinaryFiles,
+  ) => {
+    const data: EditorData = {
+      elements: excalidrawElements,
+      appState,
+      files,
+      scrollToContent: true,
+    };
+    localStorage.setItem("MEMER_EXCALIDRAW_EDITOR_DATA", JSON.stringify(data));
+  };
 
   return (
     <div className="flex flex-col h-screen">
@@ -21,7 +63,7 @@ export default function EditorPage() {
           <Loader />
         </div>
       ) : (
-        <Excalidraw />
+        <Excalidraw onChange={onChange} initialData={initialData} />
       )}
     </div>
   );
