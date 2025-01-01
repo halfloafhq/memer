022464bcd4192106eb2743e5358e850671847738
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2025-01-01 15:26:53 +0530

    add: metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,13 +9,39 @@ import { ThemeProvider } from "@/components/theme-provider";
 import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
 import { extractRouterConfig } from "uploadthing/server";
 import { ourFileRouter } from "@/app/api/uploadthing/core";
-import NextTopLoader from 'nextjs-toploader';
+import NextTopLoader from "nextjs-toploader";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Memer",
   description: "Your one-stop meme workshop: Search, collect, create.",
+  openGraph: {
+    title: "Memer by Shobhit",
+    description: "Your one-stop meme workshop: Search, collect, create.",
+    url: "https://memer.shobhitnagpal.com",
+    locale: "en_US",
+    type: "website",
+    images: [
+      {
+        url: "/memer-seo.png",
+        width: 1915,
+        height: 935,
+      },
+    ],
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
+  verification: {
+    google: "google",
+    yandex: "yandex",
+    yahoo: "yahoo",
+    other: {
+      me: ["shobhitsnagpal@gmail.com", "https://www.shobhitnagpal.com"],
+    },
+  },
 };
 
 export default function RootLayout({
