ce971bbafa0765245c051f1b1426ae93785843f0
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2025-01-08 13:33:39 +0530

    remove dashboard context

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Sidebar from "./_components/sidebar";
-import { DashboardProvider } from "@/context/DashboardContext";
 
 function DashboardLayout({
   children,
@@ -8,14 +7,12 @@ function DashboardLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <DashboardProvider>
-      <div className="flex items-start justify-between">
-        <div className="xl:min-w-[320px] hidden xl:block min-h-screen">
-         <Sidebar />
-        </div>
-        <main className="w-full h-full">{children}</main>
+    <div className="flex items-start justify-between">
+      <div className="xl:min-w-[320px] hidden xl:block min-h-screen">
+        <Sidebar />
       </div>
-    </DashboardProvider>
+      <main className="w-full h-full">{children}</main>
+    </div>
   );
 }
 
