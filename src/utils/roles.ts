2ed110aaacda44de3e6305e81fbc7bd2a91964bb
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2025-03-02 14:32:02 +0530

    init: jsdoc

--- src/utils/roles.ts
+++ src/utils/roles.ts
@@ -1,6 +1,11 @@
 import { Roles } from '@/types/globals';
 import { auth } from '@clerk/nextjs/server';
 
+/**
+ * Checks user role from session claims
+ * @param {Roles} role - User role
+ * @returns {Boolean} If user matches role or not
+ */
 export const checkRole = (role: Roles) => {
   const { sessionClaims } = auth();
 
