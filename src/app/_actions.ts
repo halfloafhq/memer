88f4568047e611c62c8134805fdfc4cc87817016
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2025-03-28 01:25:19 +0530

    add: jsdoc comments on server actions

--- src/app/_actions.ts
+++ src/app/_actions.ts
@@ -2,6 +2,10 @@
 
 import prisma from '@/lib/prisma';
 
+/**
+ * Get memes (with pagination)
+ * @param pageNumber - Page number to take data from
+ */
 export async function getMemes(pageNumber: number) {
   try {
     const memes = await prisma.meme.findMany({
@@ -18,6 +22,10 @@ export async function getMemes(pageNumber: number) {
   }
 }
 
+/**
+ * Get memes for a specific search term
+ * @param searchTerm - Query that user searched
+ */
 export async function searchMemes(searchTerm: string) {
   try {
     const memes = await prisma.meme.findMany({
@@ -45,6 +53,10 @@ export async function searchMemes(searchTerm: string) {
   }
 }
 
+/**
+ * Get collections for a user
+ * @param userId - user id
+ */
 export async function getCollections(userId: string) {
   try {
     const collections = await prisma.collection.findMany({
@@ -59,6 +71,11 @@ export async function getCollections(userId: string) {
   }
 }
 
+/**
+ * Create user in db
+ * @param email - user email
+ * @param userId - user id
+ */
 export async function createUser(email: string | null, userId: string) {
   try {
     const createdUser = await prisma.user.create({
@@ -74,6 +91,10 @@ export async function createUser(email: string | null, userId: string) {
   }
 }
 
+/**
+ * Get total count of memes
+ * @returns Promise<number> - Total number of memes
+ */
 export async function getTotalMemes() {
   try {
     const total = await prisma.meme.count();
