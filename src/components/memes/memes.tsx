5ee53ed8bd031e3f3cf55b3d47968150f486f714
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2025-03-28 02:02:58 +0530

    refactor: render save meme button if logged in and add basic motion

--- src/components/memes/memes.tsx
+++ src/components/memes/memes.tsx
@@ -2,15 +2,26 @@
 import React from 'react';
 import MemeCard from './meme-card';
 import { Meme } from '@prisma/client';
+import { motion } from 'motion/react';
 
 interface MemesProps {
   memes: Meme[];
 }
 
 export default function Memes({ memes }: MemesProps) {
-  return memes.map((meme: Meme) => (
-    <div key={meme.id}>
+  return memes.map((meme: Meme, idx: number) => (
+    <motion.div
+      key={meme.id}
+      initial={{
+        y: 25,
+      }}
+      animate={{
+        y: 0,
+        transition: { duration: 0.4 + idx },
+      }}
+      whileHover={{ scale: 1.1 }}
+    >
       <MemeCard src={meme.url} name={meme.name} description={meme.description} memeId={meme.id} />
-    </div>
+    </motion.div>
   ));
 }
